ce761b377efef30e981258e89d59b95771f2f850
JMAURICIORLIMA <mauricio.rodriguesdelima@gmail.com>
2022-03-19 01:41:03 -0300

    Autowired, Component e SpringBootApplication

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,19 +1,30 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
+import academy.devdojo.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
 public class AnimeController {
+    //Construtor
+    private final DateUtil dateUtil;
 
     //localhost:8080/anime/list
     @GetMapping
     public List<Anime> List() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Berserk"));
     }
 }
