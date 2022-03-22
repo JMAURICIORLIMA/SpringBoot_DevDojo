545d353dfcea37f9b131df8884eb5fe7a8e33347
JMAURICIORLIMA <mauricio.rodriguesdelima@gmail.com>
2022-03-22 00:39:15 -0300

    Modificação classe Anime - Anotação @DATA

--- src/main/java/academy/devdojo/springboot2/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -1,16 +1,18 @@
 package academy.devdojo.springboot2.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -21,4 +23,16 @@ public class Anime {
     private Long id;
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Anime anime = (Anime) o;
+        return id != null && Objects.equals(id, anime.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
